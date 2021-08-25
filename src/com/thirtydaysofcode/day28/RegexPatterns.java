026e2e3c5eab0dfc72e5da0d22281d3b5582868f
Tushar Pandey <tushar2898pandey@gmail.com>
2021-08-25 22:19:06 +0530

    30 Days Challenge Completed

---
+++ src/com/thirtydaysofcode/day28/RegexPatterns.java
@@ -0,0 +1,35 @@
+package com.thirtydaysofcode.day28;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPatterns {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/com/thirtydaysofcode/testcases/day28.txt"));
+        int N = sc.nextInt();
+        //Regular Expression
+        String myRegexString = ".+@gmail\\.com$";
+        //Create a Pattern Object
+        Pattern pattern = Pattern.compile(myRegexString);
+        List<String> matched = new ArrayList<>();
+        for (int i = 0; i <N; i++) {
+            String firstName = sc.next();
+            String emailID = sc.next();
+            // Matcher
+            Matcher matcher = pattern.matcher(emailID);
+            //If our matcher finds a match
+            if (matcher.find()) {
+                matched.add(firstName);
+            }
+        }
+        //Sort the list
+        Collections.sort(matched);
+        //Display the sorted result list
+        for (String s:matched){
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/com/thirtydaysofcode/day29/BitwiseAnd.java
@@ -0,0 +1,42 @@
+package com.thirtydaysofcode.day29;
+
+import java.io.*;
+import java.util.Scanner;
+
+class Result {
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                int result = i & j;
+                if (result < K && result > max) {
+                    max++;
+                }
+            }
+        }
+        return max;
+    }
+
+}
+
+public class BitwiseAnd {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/com/thirtydaysofcode/testcases/day29.txt"));
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            System.out.println(Result.bitwiseAnd(N, K));
+        }
+        sc.close();
+    }
+}
